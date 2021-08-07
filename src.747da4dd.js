863885143ff9957bbaa2e8a365b3ab37f2b69abf
riko1212 <46224717+riko1212@users.noreply.github.com>
2021-08-07 09:44:14 +0000

    Deploying to gh-pages from @ riko1212/39-2@3a0ac2cd86c5c628962b756cba4c7419f6e1c989 🚀

--- src.747da4dd.js
+++ src.747da4dd.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/39-2/src.68dbf4dc.js.map
+//# sourceMappingURL=/39-2/src.747da4dd.js.map
